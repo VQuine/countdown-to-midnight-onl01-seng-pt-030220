5115d8806d246b9913474addc9ac77708650f134
Kimberly Aviles <01010110.v@gmail.com>
2020-03-15 22:02:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,13 @@
 
 def countdown(integer_argument)
-while integer_argument > 0
-  puts "#{integer_argument} SECOND(S)!"
-  integer_argument -= 1
-end
-  puts "HAPPY NEW YEAR!"
+
+  while integer_argument > 0
+    puts "#{integer_argument} SECOND(S)!"
+    integer_argument -= 1
+  end
+
+  while integer_argument === 0
+    puts "HAPPY NEW YEAR!"
+  end
 
 end
